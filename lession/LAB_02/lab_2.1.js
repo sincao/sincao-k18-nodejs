a83484f8a162310791a453cd223ec79092d2db91
Sin <sin.cao@upmesh.io>
2025-12-04 10:58:41 -0500

    fix IBM range and change the name of day 01

--- lession/LAB_02/lab_2.1.js
+++ lession/LAB_02/lab_2.1.js
@@ -9,7 +9,7 @@ if (calculatedIBM < 18.5) {
     console.log("You are underweight. You should inscrease the weight");
 } else if (calculatedIBM >= 18.5 && calculatedIBM <= 24.9) {
     console.log("Your weight is normal. You should keep as is ");
-} else if (calculatedIBM >= 255 && calculatedIBM <= 29.9) {
+} else if (calculatedIBM >= 25 && calculatedIBM <= 29.9) {
     console.log("Your are overweight. You should do more exercise to lose weight");
 } else {
     console.log("You are obesity")
