1b06b31d464a3950aa7aaca0890b1cc83706a273
Sin <sin.cao@upmesh.io>
2025-12-13 23:08:02 -0500

    Update testcase after watch day04

--- lession/Day_02/lab_2.1.js
+++ lession/Day_02/lab_2.1.js
@@ -3,15 +3,21 @@ const readline = require("readline-sync");
 
 let userHeight = Number(readline.question("Input user's height(m): "));
 let userWeight = Number(readline.question("Input user's weight(kg): "));
-let calculatedIBM = userWeight / (userHeight * userHeight);
-calculatedIBM.toFixed(1)
 
-if (calculatedIBM < 18.5) {
+if (Number.isNaN(userHeight) || Number.isNaN(userWeight)) {
+  console.log("The inputed value is not a number. Please input again");
+}else{
+  let calculatedIBM = userWeight / (userHeight * userHeight);
+  calculatedIBM.toFixed(1);
+  if (calculatedIBM < 18.5) {
     console.log("You are underweight. You should inscrease the weight");
-} else if (calculatedIBM >= 18.5 && calculatedIBM <= 24.9) {
+  }else if(calculatedIBM >= 18.5 && calculatedIBM <= 24.9) {
     console.log("Your weight is normal. You should keep as is ");
-} else if (calculatedIBM >= 25 && calculatedIBM <= 29.9) {
-    console.log("Your are overweight. You should do more exercise to lose weight");
-} else {
-    console.log("You are obesity")
+  }else if(calculatedIBM >= 25 && calculatedIBM <= 29.9) {
+    console.log(
+      "Your are overweight. You should do more exercise to lose weight"
+    );
+  }else {
+    console.log("You are obesity");
+  }
 }
--- lession/Day_02/lab_2.2.js
+++ lession/Day_02/lab_2.2.js
@@ -2,8 +2,10 @@ const readline = require("readline-sync");
 
 let number = Number(readline.question("Please input a number: "));
 
-if (number % 2 == 0) {
-    console.log("this is even number")
+if (Number.isNaN(number)) {
+  console.log("The inputed value is not a number");
+}else if (number % 2 == 0) {
+  console.log("this is even number");
 } else {
-    console.log("this is odd number")
+  console.log("this is odd number");
 }
